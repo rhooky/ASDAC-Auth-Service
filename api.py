88cd644eab5515ef4cb44205f79e5b53561d296b
arhook <rhooky@gmail.com>
2019-05-30 13:14:22 +1000

    api update
    added code to test initial api

--- api.py
+++ api.py
@@ -1,11 +1,19 @@
 from flask import Flask, jsonify, request
 import os
+from controller.py import *
+
 
 app = Flask(__name__)
 
-session = {username:"", passsword =""}
 
-@app.route('/api/v1/login',methods=['GET'])
-def login():
-    response = {'user' : user}
+
+@app.route('/api/v1/authenticate',methods=['GET'])
+def auth():
+    response = {'status' : 'Up and running'}
     return jsonify(response)
+    code = 202
+
+
+if __name__ == "__main__":
+    app.run(debug=False,host='0.0.0.0', port=int(os.getenv('PORT', '5000')))
+    
