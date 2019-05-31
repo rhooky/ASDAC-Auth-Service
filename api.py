c38a97737419eb46d7bd2abae806b27d6781f922
arhook <rhooky@gmail.com>
2019-05-31 14:05:05 +1000

    Update api.py

--- api.py
+++ api.py
@@ -7,6 +7,14 @@ import model
 app = Flask(__name__)
 
 
+model.deleteUser("tim")
+model.deleteUser("bob")
+model.deleteUser("berryl")
+model.createUser("tim", "password", "tim@email.com", "1234", "admin", "email")
+model.createUser("bob", "password", "bob@email.com", "1234", "user", "email")
+model.createUser("berryl", "password", "berryl@email.com", "1234", "handler", "phone")
+
+
 @app.route('/api/v1/authenticate',methods=['GET'])
 def auth():
     
@@ -28,7 +36,7 @@ def ping():
     
     return "Online"
     code = 202
-    
+
 #initiates flask server
 if __name__ == "__main__":
     app.run(debug=False,host='0.0.0.0', port=int(os.getenv('PORT', '5000')))
